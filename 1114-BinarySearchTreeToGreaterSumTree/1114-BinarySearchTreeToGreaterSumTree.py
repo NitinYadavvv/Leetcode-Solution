686152eb4fc2674d80d1c9f3325af4b4d2fc2478
NitinYadavvv <nitiny1929@gmail.com>
2025-12-10 08:21:21 +0530

    Create 1114-BinarySearchTreeToGreaterSumTree

---
+++ 1114-BinarySearchTreeToGreaterSumTree/1114-BinarySearchTreeToGreaterSumTree.py
@@ -0,0 +1,27 @@
+# Last updated: 12/10/2025, 8:21:29 AM
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def bstToGst(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        self.total = 0
+        
+        def ans(root):
+
+            if root is None:
+                return 
+            ans(root.right)
+            root.val+=self.total
+            self.total = root.val
+            ans(root.left)
+        
+        ans(root)
+        return root
+        
