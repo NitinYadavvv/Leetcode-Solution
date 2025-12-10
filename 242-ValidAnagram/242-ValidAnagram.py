15de52e3aa9e563b90f24d668cc1387339e7867a
NitinYadavvv <nitiny1929@gmail.com>
2025-12-10 08:21:53 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,9 @@
+# Last updated: 12/10/2025, 8:22:02 AM
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
