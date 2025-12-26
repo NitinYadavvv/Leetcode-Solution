6e4869f705f5277b93dba25967dd14cb85a478bd
NitinYadavvv <nitiny1929@gmail.com>
2025-12-26 10:37:50 +0530

    Create 2235-AddTwoIntegers

---
+++ 2235-AddTwoIntegers/version/python/2235-AddTwoIntegers_20251226_103750.py
@@ -0,0 +1,10 @@
+# Last updated: 12/26/2025, 10:37:50 AM
+1class Solution(object):
+2    def sum(self, num1, num2):
+3        """
+4        :type num1: int
+5        :type num2: int
+6        :rtype: int
+7        """
+8        return num1 + num2
+9        
