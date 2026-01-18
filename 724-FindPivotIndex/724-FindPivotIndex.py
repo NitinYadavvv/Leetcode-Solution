535231deee7df07e22871b2293113764067ddbb0
Nitin Yadav <nitiny1929@gmail.com>
2026-01-18 18:18:46 +0530

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/724-FindPivotIndex.py
@@ -0,0 +1,19 @@
+# Last updated: 1/18/2026, 6:18:49 PM
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = 0
+
+        left = sum(nums)    
+        for i in range(len(nums)):
+         
+            left-=nums[i]
+            if left == right:
+                return i
+            right+=nums[i]
+
+        return -1
